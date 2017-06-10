f8740d54a4e60088ef0875e1c270f4ab74ee4842
alistairholmes <alistairholmes95@gmail.com>
2017-06-10 19:52:42 +0200

    Added order summary when order button is pressed. Also added checkbox for topping.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 /**
  * This app displays an order form to order coffee.
@@ -26,10 +30,31 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity*5;
-        String priceMessage = "Total = " + "$" + price;
-        priceMessage = priceMessage + "\nThank You!";
-        displayMessage(priceMessage);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price, hasWhippedCream));
+    }
+
+    /**
+     * Calculates the price of the order.
+     *@return the total price of order.
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /* Creates a summary of the order.
+     *@param takes price from calculatePrice method.
+     *@return string summary of order.
+     */
+    private String createOrderSummary(int price, boolean hasWhippedCream) {
+        String orderSummary = "Name: Alistair Holmes";
+        orderSummary += "\nAdded Whipped Cream? " + hasWhippedCream;
+        orderSummary += "\nQuantity: " + quantity;
+        orderSummary += "\nTotal: " + price;
+        orderSummary += "\nThank You!";
+        return orderSummary;
     }
 
     /**
@@ -37,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -45,31 +70,23 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int quantity) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        quantityTextView.setText("" + quantity);
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
-
+    
 }
