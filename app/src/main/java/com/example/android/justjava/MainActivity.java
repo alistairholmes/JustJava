f7a1b309c934d415104a050053949b5649de0ea4
alistairholmes <alistairholmes95@gmail.com>
2017-06-11 16:56:58 +0200

    Name field was added to collect user's name. Another checkbox for chocolate topping. Adjusted price for added toppings. Modified the order summary.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -30,27 +32,57 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        //Find out the user's name
+        EditText nameEditTextView = (EditText) findViewById(R.id.name_edittext_view);
+        Editable customerName = nameEditTextView.getText();
+
+        //Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price, hasWhippedCream));
+
+        //Figure out if the user wants chocolate topping
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        //Calculate total price of a cup of coffee
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        //Show order summary
+        displayMessage(createOrderSummary(customerName, price, hasWhippedCream, hasChocolate));
     }
 
     /**
      * Calculates the price of the order.
+     * @param whippedCream tells us if the user wants cream.
+     * @param chocolate tells us if the user wants chocolate.
      *@return the total price of order.
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean whippedCream, boolean chocolate) {
+        //Price of 1 cup of coffee
+        int coffeePrice = 5;
+
+        //Add $1 if whipped cream is added
+        if (whippedCream) {
+            coffeePrice += + 1;
+        }
+
+        //Add $2 if chocolate is added
+        if (chocolate) {
+            coffeePrice += + 2;
+        }
+
+        //Calculate total price by multiplying the quantity
+        return coffeePrice * quantity;
     }
 
     /* Creates a summary of the order.
      *@param takes price from calculatePrice method.
      *@return string summary of order.
      */
-    private String createOrderSummary(int price, boolean hasWhippedCream) {
-        String orderSummary = "Name: Alistair Holmes";
+    private String createOrderSummary(Editable name,int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String orderSummary = "Name: " + name;
         orderSummary += "\nAdded Whipped Cream? " + hasWhippedCream;
+        orderSummary += "\nAdded Chocolate? " + hasChocolate;
         orderSummary += "\nQuantity: " + quantity;
         orderSummary += "\nTotal: " + price;
         orderSummary += "\nThank You!";
